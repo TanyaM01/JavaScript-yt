4485f3cd9c5328aaa5b96cdd47c0db11a6dcb375
TanyaM01 <88895596+TanyaM01@users.noreply.github.com>
2023-12-27 12:32:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+let name = "tanya" //str
+let age = 18; //number
+let isL0oggedin = false //boolean
+let state ; //undefined
+let temp=null; //(empty)
+
+//PRIMITIVE DATA TYPES
+//number=> 2 to power 53 (range)
+//bigint =>
+//string => ""
+//boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol =>(used for uniqueness)
+
+//object
+
+console.log(typeof "tanya") //string
+console.log(typeof null) //object   (null is an object) 
+console.log(typeof undefined) //undefined
+
