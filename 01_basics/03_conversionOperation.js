bf4ad7d751b51720545f16b0700b5721c4da1afa
TanyaM01 <88895596+TanyaM01@users.noreply.github.com>
2023-12-27 17:11:59 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -66,3 +66,51 @@ let someNumber = 33;
 let stringNumber = String(someNumber)
 console.log(stringNumber);//33
 console.log(typeof stringNumber);//string
+
+
+//*****************OPERATIONS */
+
+let value = 3
+let negValue = -value;
+console.log(negValue); //-3
+
+/*
+console.log(2+2); 
+console.log(2-2); 
+console.log(2*2); 
+console.log(2**3); 
+console.log(2/3); 
+console.log(2%3); 
+*/
+
+let str1 = "Hello"
+let str2 = " Tanya"
+let str3 = str1 + str2
+console.log(str3);//Hello Tanya
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+ 2 + 2) //122
+console.log(1+ 2 + "2") //32
+
+console.log(+true);//1
+console.log(true);//true
+
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);//101
+
+let gameCounter2 = 100;
+++gameCounter2;
+console.log(gameCounter2);//101
+
+
+
+
+
+
+
