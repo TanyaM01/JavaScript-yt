42a78ddb76196ffbd1ee6738659b40a7a3ee0990
TanyaM01 <88895596+TanyaM01@users.noreply.github.com>
2023-12-30 11:44:21 +0000

    scopes n functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -23,3 +23,52 @@ console.log(p);
 INNER 10
 300
 */
+
+/////**NESTED SCOPES////////////// */ small kids can ask icecream from elders but not vice-versa
+//CLOSURE/child func can access parent variable but parent function can't access child variable
+function one(){
+    const username="hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website)   -> (error scope closed so comment this line)
+    two()
+}
+one() //hitesh
+
+if(true){
+    const username = "tanya"
+    if(username === "tanya"){
+        const website=" youtube"
+        console.log(username + website);
+    }
+    //console.log(website) ->error scope closed in if loop
+}
+//console.log(username) -> error
+
+//output-> tanya youtube
+
+
+//////////////+++++++++++++++++++++interesting+++++++++++++///////////
+
+console.log(addone(5))  //6
+function addone(num){   //function declaration only
+    return num+1
+}
+
+
+// addTwo(5)  -> ERROR(Cannot access 'addTwo' before initialization)
+const addTwo = function(num){  //function declaration plus holding in a variable
+    return num +2
+}
+
+
+
+
+
+
+
+
+
