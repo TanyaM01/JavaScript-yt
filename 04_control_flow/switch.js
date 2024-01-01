31f7bab9f7967af290b63a8872be46ff272f77ab
TanyaM01 <88895596+TanyaM01@users.noreply.github.com>
2024-01-01 09:05:47 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,62 @@
+/* syntax: 
+switch (key){
+    case value:
+
+        break;
+
+    default:
+        break;
+}
+*/
+const month = 3
+switch(month){
+    case 1:
+        console.log("january");
+        break;    //shift+alt+downkey (to copy lines of code)
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+
+}
+
+const month_name = "january"
+switch(month_name){
+    case "january":
+        console.log("january");
+        break;    //shift+alt+downkey (to copy lines of code)
+    case "feb":
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "apr":
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
