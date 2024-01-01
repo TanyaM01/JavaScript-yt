d9bb93ec77e40b21614801fb02020cc452073ae5
TanyaM01 <88895596+TanyaM01@users.noreply.github.com>
2024-01-01 12:18:36 +0000

    for each

---
+++ 05_iterations/five.js
@@ -0,0 +1,67 @@
+//For Each Loop
+const coding = ["js", "ruby", "java", "python", "cpp"]
+/*
+coding.forEach( function (item) {
+    console.log(item)
+} )
+*/
+
+coding.forEach( (item) => {
+    console.log(item)
+})
+/*js
+ruby
+java
+python
+cpp*/
+
+//now if we write a function printme, can we pass on this funcn?
+function printMe(item){
+    console.log(item)
+}
+coding.forEach(printMe)
+/*js
+ruby
+java
+python
+cpp*/
+
+
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr)
+})
+/*
+js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+*/
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName : "java",
+        languageFileName: "java"
+    },
+    {
+        languageName : "python",
+        languageFileName: "py"
+    }
+]
+myCoding.forEach( (item) => {
+    console.log(item. languageName)
+})
+/*
+javascript
+java
+python
+*/
+
+
+
+
